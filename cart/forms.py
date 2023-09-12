971d73c99803ca1ba3f729821ae1a9a251c3bda7
Alex <lenkoran26@outlook.com>
2023-09-12 21:51:24 +0300

    12.09

--- cart/forms.py
+++ cart/forms.py
@@ -1,9 +1,17 @@
 from django import forms
 
 
-QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 10)]
+# QUANTITY_CHOICES = [(i, i) for i in range(1, 10)]
+# QUANTITY_CHOICES = [
+#     (1, '1'),
+#     (2, '2'),
+#     (3, '3'),
+#     ...
+#     (9, '9'),
+# ]
 
 class CartAddProductForm(forms.Form):
-    quantity = forms.TypedChoiceField(choices=QUANTITY_CHOICES, coerce=int, label='Количество')
+    # quantity = forms.TypedChoiceField(choices=QUANTITY_CHOICES, coerce=int, label='Количество')
+    quantity = forms.IntegerField(max_value=1000, label='Количество')
     override = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
     
--- cart/urls.py
+++ cart/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import *
+from .views import cart_add, cart_remove, cart_detail
 
 
 
