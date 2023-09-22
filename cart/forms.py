a95dfdd0cffc01a554b593d227cf7d0fab989745
lenkoran26 <lenkoran26@outlook.com>
2023-09-22 08:24:39 -0400

    cart22

--- cart/forms.py
+++ cart/forms.py
@@ -1,17 +1,14 @@
 from django import forms
 
 
-# QUANTITY_CHOICES = [(i, i) for i in range(1, 10)]
-# QUANTITY_CHOICES = [
-#     (1, '1'),
-#     (2, '2'),
-#     (3, '3'),
-#     ...
-#     (9, '9'),
-# ]
-
 class CartAddProductForm(forms.Form):
-    # quantity = forms.TypedChoiceField(choices=QUANTITY_CHOICES, coerce=int, label='Количество')
-    quantity = forms.IntegerField(max_value=1000, label='Количество')
+    
+    quantity = forms.IntegerField(
+        max_value=1000, 
+        min_value=1,
+        label='Количество',
+        widget=forms.NumberInput(attrs={'style':'width:80px', 'id':'quantity', 'onchange':"sendCount(this)"}),
+        initial=1
+        )
     override = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
     
--- cart/models.py
+++ cart/models.py
@@ -4,10 +4,10 @@ from products.models import Products
 
 
 class CartUser(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 
 class CartItem(models.Model):
-    cart = models.ForeignKey(CartUser, on_delete=models.CASCADE)
-    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    cart = models.ForeignKey(CartUser, on_delete=models.CASCADE, related_name='cart')
+    product = models.ForeignKey(Products, on_delete=models.CASCADE, related_name='products')
     quantity = models.IntegerField()
