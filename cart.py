1cb479684ae827e2315b0d7bbbdb6b5dd669697b
lenkoran26 <lenkoran26@outlook.com>
2023-09-22 08:24:39 -0400

    cart22

--- cart.py
+++ cart.py
@@ -1,114 +1,41 @@
-from market import settings
+forms.py
+
+from django import forms
+
+
+class CartAddProductForm(forms.Form):
+    
+    quantity = forms.IntegerField(
+        max_value=1000, 
+        min_value=1,
+        label='Количество',
+        widget=forms.NumberInput(attrs={'style':'width:80px', 'id':'quantity', 'onchange':"sendCount(this)"}),
+        initial=1
+        )
+    override = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
+
+
+
+
+models.py
+
+from django.db import models
+from django.contrib.auth.models import User
 from products.models import Products
-from decimal import Decimal
-from django.shortcuts import render, redirect, get_object_or_404
-from django.views.decorators.http import require_POST
-from products.models import Products
-from .forms import CartAddProductForm
-from cart.models import CartUser, CartItem
-
-# Create your views here.
-
-class Cart:
-    def __init__(self, request):
-        self.session = request.session
-        self.user = request.user
-        cart = self.session.get(settings.CART_SESSION_ID)
-        if not cart:
-            cart = self.session[settings.CART_SESSION_ID] = {}
-        
-        self.cart = cart
 
 
-    def add(self, product, quantity=1, override_quantity=False):
-        product_id = str(product.id)
-        if product_id not in self.cart:
-            self.cart[product_id] = {
-                'quantity': 0,
-                'price': str(product.price)
-            }
-        if override_quantity:
-            self.cart[product_id]['quantity'] = quantity
-        else:
-            self.cart[product_id]['quantity'] += quantity
-        self.save()
+class CartUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 
-    def save(self):
-        self.session.modified = True
-    
-
-    def remove(self, product):
-        product_id = str(product.id)
-        if product_id in self.cart:
-            del self.cart[product_id]
-            self.save()
-
-    
-    def __iter__(self):
-        product_ids = self.cart.keys()
-        products = Products.objects.filter(id__in=product_ids)
-        cart = self.cart.copy()
-
-        for product in products:
-            cart[str(product.id)]['product'] = product
-        
-        for item in cart.values():
-            item['price'] = Decimal(item['price'])
-            item['total_price'] = item['price'] * item['quantity']
-            yield item
-      
-   
-    def __len__(self):
-        return sum(item['quantity'] for item in self.cart.values())
-    
-
-    def get_total_price(self):
-        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())  
-    
-
-    def clear(self):
-        del self.session[settings.CART_SESSION_ID]
-        self.save()
+class CartItem(models.Model):
+    cart = models.ForeignKey(CartUser, on_delete=models.CASCADE, related_name='cart')
+    product = models.ForeignKey(Products, on_delete=models.CASCADE, related_name='products')
+    quantity = models.IntegerField()
 
 
-@require_POST
-def cart_add(request, product_id):
-    if request.user.id:
-        return add_cart_db(request, product_id)
-    
-    cart = Cart(request)
-    product = get_object_or_404(Products, id=product_id)
-    form = CartAddProductForm(request.POST)
-    
-        
-    if form.is_valid():
-        cd = form.cleaned_data
-        cart.add(product=product, 
-                 quantity=cd['quantity'],
-                 override_quantity=cd['override'])
-    
-    return redirect('cart:cart-detail')
 
-
-@require_POST
-def cart_remove(request, product_id):
-    cart = Cart(request)
-    product = get_object_or_404(Products, id=product_id)
-    cart.remove(product)
-    
-    return redirect('cart:cart-detail')
-
-
-def cart_detail(request):
-    cart = Cart(request)
-    for item in cart:
-        item['update_quantity_form'] = CartAddProductForm(initial={
-            'quantity': item['quantity'],
-            'override': True})
-    return render(request, 'cart/cart-detail.html', {'cart': cart})
-
-
+views.py
 
 # корзина для авторизованного пользователя (хранится постоянно  в БД)
 class ProductCartUser:
@@ -243,3 +170,4 @@ def remove_from_db(request, product_id):
     cart.remove(product_id, request)    
     
     return redirect('cart:cart-detail')
+
