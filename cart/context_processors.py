129f0fa569e9b39c05843259fd9e1ec968995a81
lenkoran26 <lenkoran26@outlook.com>
2023-09-27 09:30:58 -0400

    order-list

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -1,5 +1,8 @@
-from .views import Cart
+from .views import Cart, ProductCartUser
 
 
 def cart(request):
+    if request.user.id:
+        return {'cart': ProductCartUser(request)}
+    
     return {'cart': Cart(request)}
--- orders/views.py
+++ orders/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Order, OrderItem
 from products.models import Products
 from .forms import OrderForm
+from django.views.generic import ListView
 
 # создание заказа
 def create_order(request):
@@ -26,13 +27,24 @@ def create_order(request):
         order_num = order_form.instance.number
         # получаем объект самого заказа из БД
         order = Order.objects.get(number = order_num)
+        total_price = 0
         
         # проходимся по корзине и заносим товары в заказ
         for product_id in cart.keys():
             product = Products.objects.get(pk=product_id)
             quantity = cart[product_id]['quantity']
-        
+            total_price += product.price * int(quantity)
+            # создаем позицию товара в заказе
             OrderItem.objects.create(order=order, product=product, quantity=quantity)
+        
+        order.total_price = total_price
+        cart.clear()
+        context = {'order': order}
+        
+        return render(request, 'orders/order_success.html', context=context)
             
-
+            
+class OrdersListView(ListView):
+    model = Order
+    template_name = 'orders/user_orders.html'
     
